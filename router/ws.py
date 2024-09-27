70d33df334d6f5cd087de9c545d652524387b57a
Akhilesh M T <akhilesh@trigeminal-ai.com>
2024-09-27 11:10:29 +0000

    functionality completed

--- router/ws.py
+++ router/ws.py
@@ -12,7 +12,8 @@ def get_user_id_from_token():
 
 @router.websocket("")
 async def connect(websocket: WebSocket, user_id=Depends(get_user_id_from_token)):
-    await websocket.accept()
-    # await wsok_manager.connect(websocket, user_id)
+    # await websocket.accept()
+    await wsok_manager.connect(websocket, user_id)
     # await wsok_manager.ping(websocket)
     # await wsok_manager.pong(websocket)
+    # await wsok_manager.
